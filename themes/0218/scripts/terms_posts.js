307e2da6c09465e8c013eb5115bb3ecc04c3af1e
hiro0218 <hiro0218@gmail.com>
2020-04-24 23:26:42 +0900

    fix: 関数化して分離

--- themes/0218/scripts/terms_posts.js
+++ themes/0218/scripts/terms_posts.js
@@ -1,15 +1,14 @@
-hexo.extend.generator.register('tags_posts', (locals) => {
-  const tags = locals.tags;
+const getTermsPosts = (terms) => {
   const data = [];
 
-  for (let i = 0; i < tags.length; i++) {
-    const tag = tags.data[i];
-    const posts = tag.posts;
+  for (let i = 0; i < terms.length; i++) {
+    const term = terms.data[i];
+    const posts = term.posts;
 
     data.push({
-      name: tag.name,
-      slug: tag.slug,
-      count: tag.length,
+      name: term.name,
+      slug: term.slug,
+      count: term.length,
       posts: [],
     });
 
@@ -21,6 +20,13 @@ hexo.extend.generator.register('tags_posts', (locals) => {
       });
     }
   }
+
+  return data;
+};
+
+hexo.extend.generator.register('tags_posts', (locals) => {
+  const tags = locals.tags;
+  const data = getTermsPosts(tags);
 
   return {
     path: 'tags_posts.json',
@@ -30,27 +36,7 @@ hexo.extend.generator.register('tags_posts', (locals) => {
 
 hexo.extend.generator.register('categories_posts', (locals) => {
   const categories = locals.categories;
-  const data = [];
-
-  for (let i = 0; i < categories.length; i++) {
-    const category = categories.data[i];
-    const posts = category.posts;
-
-    data.push({
-      name: category.name,
-      slug: category.slug,
-      count: category.length,
-      posts: [],
-    });
-
-    for (let j = 0; j < posts.length; j++) {
-      const post = posts.data[j];
-      data[i].posts.push({
-        title: post.title,
-        path: encodeURI(post.path),
-      });
-    }
-  }
+  const data = getTermsPosts(categories);
 
   return {
     path: 'categories_posts.json',
